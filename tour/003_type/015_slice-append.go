d27d464612dcaffc9e6763d1ec578e1806f1a941
Hiroyuki Aoki <aokiplayer@icloud.com>
2021-10-22 15:08:23 +0900

    append で、 slice に要素を追加できる 複数の要素を追加した場合だけは、 capacity が多めに確保される？

---
+++ tour/003_type/015_slice-append.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	s = append(s, 0)
+	printSlice(s)
+
+	s = append(s, 1)
+	printSlice(s)
+
+	// 複数の要素を追加した場合だけは、 capacity が多めに確保される？
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
