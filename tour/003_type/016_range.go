99a45b1ff7fce9106e27dd239a9fdb947569f232
Hiroyuki Aoki <aokiplayer@icloud.com>
2021-10-22 15:24:11 +0900

    range スライス とすると、「要素のインデックス」と「要素のコピー」の 2 つが取れる

--- tour/003_type/016_range.go
+++ tour/003_type/016_range.go
@@ -6,7 +6,7 @@ var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
 	// for で利用する range は、 slice や map をひとつずつ反復処理するために利用
-	// range スライス とすると、要素のインデックスと要素の 2 つが取れる？
+	// range スライス とすると、「要素のインデックス」と「要素のコピー」の 2 つが取れる
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
